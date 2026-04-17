a339648d3eb992b503dd2be5d91e1eca0548c365
AndresHuilinir <andres.huilinir@usm.cl>
2026-04-17 17:04:39 -0400

    Por si acaso

--- Tarea 1/Tarea_1.py
+++ Tarea 1/Tarea_1.py
@@ -1,17 +1,17 @@
 import numpy as np
 
-#La funcion por defecto revisa que se trunque para >0 y si no
-#es un valor de conteo (num enteros) hay que avisarle con 0
-
 numero_de_grupo = 3
 
 rng = np.random.default_rng(numero_de_grupo)
 
-def generar_normal(mu, sigma, limite_inferior=0, es_entero=True):
+def generar_normal(mu, sigma, limite_inferior=0, es_entero=True, limite_superior=None):
     valor = rng.normal(mu, sigma)
     if es_entero:
         valor = round(valor)
-    return max(limite_inferior, valor)
+    valor = max(limite_inferior, valor)
+    if limite_superior is not None:
+        valor = min(limite_superior, valor)
+    return valor
 
 def generar_uniforme(a, b, es_entero=False):
     valor = rng.uniform(a, b)
@@ -19,18 +19,16 @@ def generar_uniforme(a, b, es_entero=False):
         valor = max(0, round(valor))
     return valor
 
-
 T= 24
-p = 3 # cantidad de plantas
-k = 2 # tipo de maquina
-M_0 = 5 # Cantidad de operarios inicial
-# Definir Y_0k, que es la cantidad inicial de maquinas
-# instaladas de cada tipo
+p = 3 
+k = 2 
+M_0 = 8 
+Y_01 = 2 
+Y_02 = 1
+
 H = 480
-d_1 = generar_normal(2500,0.05*25000,1)
-r_t = generar_uniforme(1.02,1.04)
-#Acá debo crear una lista para crear el resto de d_t
-#Va del 2 al 24
+d_1 = generar_normal(2500, 0.05*25000, 1)
+r_t = generar_uniforme(1.02, 1.04)
 d_t = [d_1]
 
 print(f"d_1 = {d_1}")
@@ -40,21 +38,41 @@ for t in range(2, T+1):
     d_siguiente = round(d_t[-1] * r_t)
     d_t.append(d_siguiente)
     print(f"d_{t} = {d_t[-1]}")
-    
     r_t = generar_uniforme(1.02, 1.04)
     print(f"r_{t} = {r_t}\n")
 
-p_1 = generar_normal(6000,0.05*6000)
-p_2 = generar_normal(11500,0.05*11500)
-alfa = generar_normal(0.1,0.15*0.1)
+p_1 = generar_normal(6000, 0.05*6000)
+p_2 = generar_normal(11500, 0.05*11500)
+alfa = generar_normal(0.1, 0.15*0.1, es_entero=False)
+
 S_p = []
 for p_ in range(p):
-    S_p[p_] = generar_normal(100,0.05*100)
+    S_p.append(generar_normal(100,0.05*100))
 
 u = 0.7
 S_yk = []
 for k_ in range(k):
-    S_yk[k_] = generar_normal(15,0.05*15)
+    S_yk.append(generar_normal(15,0.05*15))
+
+S_m = []
+for m_ in range(M_0):
+    S_m.append(generar_normal(6,6*0.05))
+
+V_max = 0.1
+O_max = 0.3
+
+C_m11 = generar_normal(18000000,18000000*0.05)
+C_m21 = generar_normal(32000000,32000000*0.05)
+C_hire1 = generar_normal(500000,500000*0.05)
+C_fire1 = generar_normal(1800000,0.05*1800000)
+C_sal1 = generar_normal(7200000,0.03*7200000)
+C_r1 = generar_normal(15000000,0.05*15000000)
+C_I1 = generar_normal(45000000,0.05*45000000)
+C_p1 = generar_normal(2500,0.05*2500)
+C_lost1 = generar_normal(1200,0.05*1200)
+
+InfProm = generar_normal(0.04,0.002*0.002,0.03,False, 0.05)
 
 print(f"p_1 = {p_1}")
 print(f"p_2 = {p_2}")
+print(f"alfa = {alfa}")
