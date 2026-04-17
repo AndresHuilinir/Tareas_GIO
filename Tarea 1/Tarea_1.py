32405dc56449333218d731baedd58895bff8c125
AndresHuilinir <andres.huilinir@usm.cl>
2026-04-17 17:57:49 -0400

    Penultimos cambios

--- Tarea 1/Tarea_1.py
+++ Tarea 1/Tarea_1.py
@@ -27,7 +27,7 @@ Y_01 = 2
 Y_02 = 1
 
 H = 480
-d_1 = generar_normal(2500, 0.05*25000, 1)
+d_1 = generar_normal(2500,0.05*25000,1)
 r_t = generar_uniforme(1.02, 1.04)
 d_t = [d_1]
 
@@ -41,9 +41,9 @@ for t in range(2, T+1):
     r_t = generar_uniforme(1.02, 1.04)
     print(f"r_{t} = {r_t}\n")
 
-p_1 = generar_normal(6000, 0.05*6000)
-p_2 = generar_normal(11500, 0.05*11500)
-alfa = generar_normal(0.1, 0.15*0.1, es_entero=False)
+p_1 = generar_normal(6000,0.05*6000)
+p_2 = generar_normal(11500,0.05*11500)
+alfa = generar_normal(0.1,0.15*0.1,es_entero=False)
 
 S_p = []
 for p_ in range(p):
@@ -71,7 +71,12 @@ C_I1 = generar_normal(45000000,0.05*45000000)
 C_p1 = generar_normal(2500,0.05*2500)
 C_lost1 = generar_normal(1200,0.05*1200)
 
+#dado que cada t es un trimestre, el periodo total son 8 años
 InfProm = generar_normal(0.04,0.002*0.002,0.03,False, 0.05)
+R = generar_normal(0.10,0.005*0.005,0.09,False, 0.11)
+
+pi_min = (1+InfProm)**0.25 -1
+r = (1+R)**0.25 -1
 
 print(f"p_1 = {p_1}")
 print(f"p_2 = {p_2}")
